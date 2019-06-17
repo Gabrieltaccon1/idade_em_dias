c9e04d45f6871a9d4ab9e74c7648b5d7b603e5e3
Gabriel Bernardes Tacconi Lourenço <gabrieltacconi147@gmail.com>
2019-06-17 14:36:32 -0300

    dias anos

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace uri1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade, anos, dias, meses, resto;
+
+            Console.WriteLine("DIGITE SUA IDADE EM DIAS");
+            idade = int.Parse(Console.ReadLine());
+
+            anos = idade / 365;
+            resto = idade % 365;
+            meses = resto / 30;
+            dias = resto % 30;
+
+            Console.WriteLine("anos:" + anos);
+            Console.WriteLine("meses:" + meses);
+            Console.WriteLine("dias:" + dias);
+
+
+        }
+    }
+}
